59574a2e32b8908a4dd9e5c68f71c49b05271357
mtcarilli <68964724+mtcarilli@users.noreply.github.com>
2025-01-20 13:16:21 -0800

    Update bivae.py

--- BIVI/BIVI/bivae.py
+++ BIVI/BIVI/bivae.py
@@ -16,7 +16,7 @@ from torch.distributions import Normal
 from torch.distributions import kl_divergence as kl
 
 from scvi import REGISTRY_KEYS
-from scvi._compat import Literal
+from typing import Literal
 from scvi.distributions import NegativeBinomial, Poisson, ZeroInflatedNegativeBinomial
 from scvi.module.base import BaseLatentModeModuleClass, LossRecorder, auto_move_data
 from scvi.nn import DecoderSCVI, Encoder, LinearDecoderSCVI, one_hot
